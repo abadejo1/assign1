0dad963a52e55200b048269a4ed9b3f726e4dd59
EC2 Default User <ec2-user@ip-172-31-10-85.ec2.internal>
2023-03-20 20:38:59 +0000

    db & app file

---
+++ terraform/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Albert"
+    "App"   = "Web"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "week1"
+}
